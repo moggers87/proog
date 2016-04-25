8615907d891f3219bfbf9df1078873bd01003b92
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-04-25 21:54:47 +0100

    Queue

--- proog/queues.py
+++ proog/queues.py
@@ -1,3 +1,16 @@
 """
 Various implementations of a queue
 """
+
+class Queue:
+    filename = None
+
+    """An in-memory FIFO that syncs to disk"""
+    def push(self, message):
+        """Push a message onto the queue"""
+
+    def pop(self):
+        """Pop a message off the queue"""
+
+    def sync(self):
+        """Sync the queue to disk"""
