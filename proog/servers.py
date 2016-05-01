701d3f7934c6cf7850c199ad8b74d8451c468f5b
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2016-05-01 23:30:17 +0100

    Make EHLO be smart about what features are available

--- proog/servers.py
+++ proog/servers.py
@@ -15,6 +15,51 @@ class BaseServer:
     """Overrides some of aiosmtpd's SMTP class, but don't inherit from it yet
     so we can mix it up with the LMTP class too"""
 
+    _ehlo_features = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._ehlo_features is None:
+            self._ehlo_features = []
+
+        # XXX command_size_limits should go in EHLO?
+        if self.data_size_limit:
+            self._ehlo_features.append("SIZE %s"% self.data_size_limit)
+             self.command_size_limits['MAIL'] += 26
+
+        if not self._decode_data:
+            self._ehlo_features.append("8BITMIME"])
+
+        # XXX command_size_limits should go in EHLO?
+        if self.enable_SMTPUTF8:
+            self._ehlo_features.append("SMTPUTF8")
+            self.command_size_limits['MAIL'] += 10
+
+    @asyncio.coroutine
+    def smtp_EHLO(self, hostname):
+        if not hostname:
+            yield from self.push("501 Syntax: EHLO hostname")
+            return
+        if self.seen_greeting:
+            yield from self.push("503 Duplicate HELO/EHLO")
+            return
+
+        self._set_rset_state()
+        self.seen_greeting = hostname
+        self.extended_smtp = True
+
+        if self._ehlo_features:
+            # list features
+            yield from self.push("250-%s" % self.fqdn)
+            for feature in self._ehlo_features[:-1]:
+                yield from self.push("250-%s" % self.fqdn)
+
+            # only the last line has a space between code and arg
+            yield from self.push("250 %s" % self._ehlo_features[-1])
+        else:
+            # no features
+            yield from self.push("250 %s" % self.fqdn)
+
     @asyncio.coroutine
     def smtp_HELP(self, arg):
         # disable HELP
@@ -23,6 +68,7 @@ class BaseServer:
     @asyncio.coroutine
     def smtp_RCPT(self, arg):
         # override aiosmptd's RCPT method so we can call our event_handler
+        # XXX upstream bug as handlers should be able to do this already?
         if not self.seen_greeting:
             yield from sef.push("503 Error: send HELO/EHLO first")
             return
@@ -52,17 +98,20 @@ class BaseServer:
             status = error.message
         else:
             self.rcpttos.append(address)
-        finally:
-            self.push(status)
+        self.push(status)
 
 
 class AuthMixin:
     # XXX stub
+    # XXX how should this work with STARTTLS? e.g. auth only available tls?
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._ehlo_features.append("AUTH PLAIN LOGIN")
 
     @asyncio.coroutine
-    def smtp_EHLO(self):
-        super().smtp_EHLO()
-        self.push("250 AUTH")
+    def smtp_AUTH(self, arg):
+        pass
 
 
 class SMTP(BaseServer, aiosmtpd.SMTP):
